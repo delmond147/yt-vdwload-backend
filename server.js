b1235954512ffda2866b2bbf6873bc78eec5ae43
delmond147 <delmondbongha147@gmail.com>
2025-02-24 14:50:57 +0100

    Added playlist downloads and progress tracking

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.post('/download', async (req, res) => {
+app.post('/download', (req, res) => {
     const {url, format, quality} = req.body;
     if(!url) return res.status(400).json({ërror: 'URL is required'});	
 
@@ -19,14 +19,29 @@ app.post('/download', async (req, res) => {
     if (format === "audio") formatOption = "-f bestaudio --extract-audio --audio-format mp3";
     if (quality) formatOption = `-f "bestvideo[height<=${quality}]+bestaudio/best"`;
 
-    const command = `yt-dlp ${formatOption} -o "${outputPath}" ${url}`;
+    // const command = `yt-dlp ${formatOption} -o "${outputPath}" ${url}`;
+    let command;
+    if(type === "playlist") {
+        command = `yt-dlp ${formatOption} -o "${outputPath}"  --yes-playlist ${url}`;
+    } else {
+        command = `yt-dlp ${formatOption} -o "${outputPath}" ${url}`;
+    }
 
-    // replace the default format with the selected format
-    exec(command, (error, stdout, stderr) => {
-        if (error) return res.status(500).json({ërror: stderr});
-        res.json({message: "Download Started" });	
+    const process = exec(command);
 
-    });
+    process.stdout.on("data", (data) => {
+        console.log(data);
+        res.write(data);
+    })
+
+    process.stderr.on("data", (data) => {
+        console.error(data);
+    })
+
+    process.on("exit", () => {
+        res.end("Download Completed!")
+    })
+
 });
 
 app.listen(PORT, () => console.log("server listening on http://localhost:5000"));
