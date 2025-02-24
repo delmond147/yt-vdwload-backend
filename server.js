6b87ef37aaa1dc4cf7dcee6594c9ba9ff8f5ea87
delmond147 <delmondbongha147@gmail.com>
2025-02-24 13:02:29 +0100

    Added video quality selection and audio downloading

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const {exec} = require('child_process');
+const path = require('path');
 
 const app = express();
 PORT = 5000;
@@ -8,10 +9,19 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/download', async (req, res) => {
-    const videoUrl = req.body.url;
-    if(!videoUrl) return res.status(400).json({ërror: 'URL is required'});	
+    const {url, format, quality} = req.body;
+    if(!url) return res.status(400).json({ërror: 'URL is required'});	
 
-    const command = `youtube-dl -f best -o "downloads/%(title)s.%(ext)s" ${videoUrl}`;
+    const outputPath = path.join(__dirname, "downloads", "%(title)s.%(ext)s");
+
+    // default format selection
+    let formatOption = "-f bestvideo+bestaudio";
+    if (format === "audio") formatOption = "-f bestaudio --extract-audio --audio-format mp3";
+    if (quality) formatOption = `-f "bestvideo[height<=${quality}]+bestaudio/best"`;
+
+    const command = `yt-dlp ${formatOption} -o "${outputPath}" ${url}`;
+
+    // replace the default format with the selected format
     exec(command, (error, stdout, stderr) => {
         if (error) return res.status(500).json({ërror: stderr});
         res.json({message: "Download Started" });	
